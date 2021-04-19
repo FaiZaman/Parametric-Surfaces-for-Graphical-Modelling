ba4adea87dedb434a61452cdad39d49a9f252d14
Faiz <faiz-uz.zaman@dur.ac.uk>
2021-04-19 11:45:42 +0100

    Adjusted head for proper proportions and colour

--- human_face_model.py
+++ human_face_model.py
@@ -1,10 +1,14 @@
+from matplotlib import cm
+
 from geomdl import NURBS
 from geomdl import exchange
 from geomdl.visualization import VisMPL
 
+# import the control point data
 surface = exchange.import_json("data/head.json")
 
+# set surface parameters and generate
 surface[0].sample_size = 100
 surface[0].delta = 0.05
 surface[0].vis = VisMPL.VisSurface()
-surface[0].render()
+surface[0].render(colormap=cm.copper)
