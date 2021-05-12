89fc9b5f963e6cc4f73f3d943ae001221ee49458
Faiz <faiz-uz.zaman@dur.ac.uk>
2021-05-12 09:55:17 +0100

    Removed multiple surfaces face and created single surface face in blender

--- human_face_model.py
+++ human_face_model.py
@@ -23,43 +23,11 @@ left_face.ctrlpts = ctrl_pts
 left_face.knotvector_u = utilities.generate_knot_vector(left_face.degree_u, 3)
 left_face.knotvector_v = utilities.generate_knot_vector(left_face.degree_v, 4)
 
-# set face control points
-right_face = NURBS.Surface()
-
-right_face.degree_u = 2
-right_face.degree_v = 2
-right_face.delta = 0.05
-
-ctrl_pts = exchange.import_txt("data/right_face.cpt", separator=",")
-
-right_face.ctrlpts_size_u = 3
-right_face.ctrlpts_size_v = 4
-right_face.ctrlpts = ctrl_pts
-
-right_face.knotvector_u = utilities.generate_knot_vector(right_face.degree_u, 3)
-right_face.knotvector_v = utilities.generate_knot_vector(right_face.degree_v, 4)
-
-# set face control points
-left_eye = NURBS.Surface()
-
-left_eye.degree_u = 1
-left_eye.degree_v = 1
-left_eye.delta = 0.05
-
-ctrl_pts = exchange.import_txt("data/left_eye.cpt", separator=",")
-
-left_eye.ctrlpts_size_u = 2
-left_eye.ctrlpts_size_v = 3
-left_eye.ctrlpts = ctrl_pts
-
-left_eye.knotvector_u = utilities.generate_knot_vector(left_eye.degree_u, 2)
-left_eye.knotvector_v = utilities.generate_knot_vector(left_eye.degree_v, 3)
-
 # add to container
-human_face = multi.SurfaceContainer(left_face, right_face, left_eye)
+human_face = multi.SurfaceContainer(left_face)
 
 # render with colourmap
 human_face.vis = VisMPL.VisSurface()
-human_face.render(colormap=[cm.copper, cm.copper, cm.plasma])
+human_face.render(colormap=[cm.copper])
 
 # .samplesize - levels of detail
