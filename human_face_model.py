31862fb3963a42ebde0529b2e5e8ba6fcfa623a3
Faiz <faiz-uz.zaman@dur.ac.uk>
2021-05-12 14:16:39 +0100

    Adjusted some face control points and rendered using vtk

--- human_face_model.py
+++ human_face_model.py
@@ -1,33 +1,28 @@
 from matplotlib import cm
 
 from geomdl import NURBS
-from geomdl import BSpline
 from geomdl import multi
 from geomdl import utilities
 from geomdl import exchange
-from geomdl.visualization import VisMPL
+from geomdl.visualization import VisVTK
 
 # set face control points
-left_face = NURBS.Surface()
+face = NURBS.Surface()
 
-left_face.degree_u = 2
-left_face.degree_v = 2
-left_face.delta = 0.05
+face.degree_u = 3
+face.degree_v = 3
 
-ctrl_pts = exchange.import_txt("data/left_face.cpt", separator=",")
+ctrl_pts = exchange.import_txt("data/face.cpt", separator=",")
 
-left_face.ctrlpts_size_u = 3
-left_face.ctrlpts_size_v = 4
-left_face.ctrlpts = ctrl_pts
+face.ctrlpts_size_u = 12
+face.ctrlpts_size_v = 15
+face.ctrlpts = ctrl_pts
 
-left_face.knotvector_u = utilities.generate_knot_vector(left_face.degree_u, 3)
-left_face.knotvector_v = utilities.generate_knot_vector(left_face.degree_v, 4)
-
-# add to container
-human_face = multi.SurfaceContainer(left_face)
+face.knotvector_u = utilities.generate_knot_vector(face.degree_u, 12)
+face.knotvector_v = utilities.generate_knot_vector(face.degree_v, 15)
 
 # render with colourmap
-human_face.vis = VisMPL.VisSurface()
-human_face.render(colormap=[cm.copper])
+face.vis = VisVTK.VisSurface(ctrlpts=False, axes=False)
+face.render(cpcolor='wheat')
 
 # .samplesize - levels of detail
